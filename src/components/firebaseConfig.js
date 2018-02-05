4f2c9074be7e820a9377f4e6db0f496e8f0613d2
joMahdi <joulali.mehdi@gmail.com>
2018-02-05 04:16:55 +0000

    first commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+		apiKey: "AIzaSyB5ZCPkmZgJHgum6YNfWdPb1nq786kAcks",
+	    authDomain: "vuefs-prod-36ac9.firebaseapp.com",
+	    databaseURL: "https://vuefs-prod-36ac9.firebaseio.com",
+	    projectId: "vuefs-prod-36ac9",
+	    storageBucket: "vuefs-prod-36ac9.appspot.com",
+	    messagingSenderId: "167801663646"
+}

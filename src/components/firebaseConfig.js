630dd68c5dd86588653f20f0b93863cb23aaaad1
mahdi joulali <35194567+joMahdi@users.noreply.github.com>
2018-02-05 04:43:06 +0000

    Update firebaseConfig.js

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -1,8 +1,6 @@
 export default {
-		apiKey: "AIzaSyB5ZCPkmZgJHgum6YNfWdPb1nq786kAcks",
-	    authDomain: "vuefs-prod-36ac9.firebaseapp.com",
-	    databaseURL: "https://vuefs-prod-36ac9.firebaseio.com",
-	    projectId: "vuefs-prod-36ac9",
-	    storageBucket: "vuefs-prod-36ac9.appspot.com",
-	    messagingSenderId: "167801663646"
+		   apiKey: '<your-key>',
+       authDomain: '<your-project-authdomain>',
+       databaseURL: '<your-database-URL>',
+       storageBucket: '<your-storage-bucket>'
 }
